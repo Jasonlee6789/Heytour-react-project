c3e549b6ff57a3f96ebcbb1b21295522500e0845
Jasonlee6789 <jeffersonlee@163.com>
2020-11-29 16:16:48 +0800

    练习Grid.

--- heytour/src/authentication/LoginReducer.js
+++ heytour/src/authentication/LoginReducer.js
@@ -1,9 +1,26 @@
 export default function LoginReducer(state, action) {
   switch (action.type) {
-    case "LOGIN":
-      return action.user;
-    case "NOTLOGIN":
-      return "";
+    case "login":
+      return {
+        ...state,
+        isLoading: true,
+        error: "",
+      };
+    case "success":
+      return {
+        ...state,
+        isLoggedIn: true,
+        isLoading: false,
+      };
+    case "error":
+      return {
+        ...state,
+        error: action.payload.error,
+        name: "",
+        pwd: "",
+        isLoading: false,
+      };
+    default:
+      return state;
   }
-  return state;
 }
--- heytour/src/common/AppMenu.js
+++ heytour/src/common/AppMenu.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Menu, Button } from "semantic-ui-react";
+import React, { useState, useReducer } from "react";
+import { Menu, Button, Grid, Image } from "semantic-ui-react";
 import Login from "../authentication/Login";
+import LoginReducer from "../authentication/LoginReducer";
 
 export default function AppMenu() {
   const [loginOpen, setLoginOpen] = useState(false);
@@ -8,6 +9,16 @@ export default function AppMenu() {
   const [isUserLogin, setIsUserLogin] = useState(false);
 
   const [userName, setUserName] = useState(null);
+
+  const initState = {
+    userName: "",
+    pwd: "",
+    isLoading: false,
+    error: "",
+    isLoggedIn: false,
+  };
+
+  const [state, dispatch] = useReducer(LoginReducer, initState);
 
   function handleOpenLogin() {
     setLoginOpen(true);
@@ -19,6 +30,7 @@ export default function AppMenu() {
 
   function handleLogin(username) {
     console.log(username);
+    dispatch({ type: "login" });
     if (username) {
       setIsUserLogin(true);
       setUserName(username);
@@ -55,6 +67,32 @@ export default function AppMenu() {
           onLogin={handleLogin}
         />
       )}
+
+      <Grid celled="internally">
+        <Grid.Row>
+          <Grid.Column width={3}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/centered-paragraph.png" />
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column width={3}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
